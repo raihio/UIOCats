99c5c151180520b4fff0f26a7c12bc28b519bef1
Raihan Miah <muhamedraihan@gmail.com>
2020-08-17 16:22:11 +0100

    Finished off monoids

--- src/main/scala/MonoidCat.scala
+++ src/main/scala/MonoidCat.scala
@@ -15,6 +15,22 @@ object MonoidCat {
       }
     }
     println(add(List(None, Some(2), Some(3))))
+
+
+    import cats.instances.map._
+
+    val map1 = Map("a" -> 1, "b" -> 2)
+    val map2 = Map("b" -> 3, "d" -> 4)
+
+    println(map1 |+| map2)
+
+    import cats.instances.tuple._
+    import cats.instances.string._
+    import cats.instances.int._
+    val tuple1 = ("hello", 123)
+    val tuple2 = ("world", 321)
+
+    println(tuple1 |+| tuple2)
   }
 
   def add[A: Monoid](items: List[A]): A = {
