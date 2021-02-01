e4abf9bd1ab6f1bd64fb2a5a3b7802106f5f0896
Raihan <muhamedraihan@gmail.com>
2021-02-01 16:29:09 +0000

    Start Writer Monad

--- src/main/scala/Monad.scala
+++ src/main/scala/Monad.scala
@@ -8,7 +8,9 @@ trait Monad[F[_]] {
   }
 }
 
-import cats.Id
+import cats.{Eval, Id, MonadError}
+
+import scala.util.Try
 
 class IdMonad extends Monad[Id] {
   override def pure[A](a: A): Id[A] = a
@@ -20,4 +22,21 @@ class IdMonad extends Monad[Id] {
   override def map[A, B](value: Id[A])(func: A => B): Id[B] = {
     func(value)
   }
+}
+
+def main(args: Array[String]): Unit = {
+  import cats.syntax.applicative._
+  import cats.syntax.monadError._
+  def validateAdult[F[_]](age: Int)(implicit me: MonadError[F, Throwable]): F[Int] = {
+    val validatedAge = age.pure[F]
+    validatedAge.ensure(new IllegalArgumentException("Age must be 18 or greater"))(_ >= 18)
+  }
+
+
+  def foldRight[A, B](as: List[A], acc: B)(fn: (A, Eval[B]) => Eval[B]): Eval[B] =
+    as match {
+      case head :: tail => Eval.defer(fn(head, foldRight(tail, acc)(fn)))
+      case Nil => Eval.now(acc)
+    }
+
 }
