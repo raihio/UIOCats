92336667db2964e32621b1ae1556fb6f50c4756f
Raihan <muhamedraihan@gmail.com>
2021-02-27 14:55:36 +0000

    Doing some traversing

--- src/main/scala/Traverz.scala
+++ src/main/scala/Traverz.scala
@@ -1,11 +1,30 @@
+import cats.Foldable
+import cats.implicits.{catsKernelStdGroupForInt, catsStdInstancesForList, catsStdInstancesForVector}
 
 object Traverz {
   def main(args: Array[String]): Unit = {
-    val r = List(1, 2, 3, 4, 5).foldRight(List[Int]())(_ :: _)
-    val l = List(1, 2, 3, 4, 5).foldLeft(List[Int]().empty)((a, i) => i :: a)
 
-    println(r)
-    println(l)
+    val ints = List(Vector(1, 2, 3), Vector(4, 5, 6))
+
+    val b = (Foldable[List] compose Foldable[Vector]).combineAllOption(ints)
+    println(b)
+  }
+
+  def map[A, B](ls: List[A])(fn: A => B): List[B] = {
+    ls.foldRight(List.empty[B])((a, i) => fn(a) :: i)
+  }
+
+  def flatMap[A, B](list: List[A])(func: A => List[B]): List[B] = {
+    list.foldRight(List.empty[B])((item, acc) => func(item) ::: acc)
+  }
+
+  def filter[A](list: List[A])(func: A => Boolean): List[A] = {
+    list.foldRight(List.empty[A])((item, acc) => {
+      if (func(item)) item :: acc
+      else acc
+    })
 
   }
+
+
 }
